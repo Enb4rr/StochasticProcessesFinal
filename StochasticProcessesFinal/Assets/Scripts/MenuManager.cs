04df9089fe81d1af148ea7ade0f7a1d0fef9edde
Julian Rosero <julian.rosero@outlook.es>
2022-11-08 16:16:44 -0500

    Visual Part & other stuff

--- StochasticProcessesFinal/Assets/Scripts/MenuManager.cs
+++ StochasticProcessesFinal/Assets/Scripts/MenuManager.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Transform hidePos, menuPos;
+    public GameObject menu, options, credits;
+
+    private List<GameObject> panelsList;
+
+    private void Awake()
     {
-        
+        hidePos = GameObject.Find("HidePos").GetComponent<Transform>();
+        menuPos = GameObject.Find("MenuPos").GetComponent<Transform>();
+
+        panelsList = new List<GameObject>(3);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        panelsList.Add(menu);
+        panelsList.Add(options);
+        panelsList.Add(credits);
+    }
+
+    public void SetOptions()
+    {
+        TurnOff();
+        options.gameObject.SetActive(true);
+        options.transform.DOMove(menuPos.transform.position, 0.5f, false);
+    }
+
+    public void SetCredits()
+    {
+        TurnOff();
+        credits.gameObject.SetActive(true);
+        credits.transform.DOMove(menuPos.transform.position, 0.5f, false);
+    }
+
+    public void SetMainMenu()
+    {
+        TurnOff();
+        menu.gameObject.SetActive(true);
+        menu.transform.DOMove(menuPos.transform.position, 0.5f, false);
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void TurnOff()
+    {
+        HideAll();
+    }
+
+    private void HideAll()
+    {
+        foreach(var panel in panelsList)
+        {
+            panel.transform.DOMove(hidePos.transform.position, 0.1f, false);
+        }
     }
 }
